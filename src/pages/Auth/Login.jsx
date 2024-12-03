a66e016bc4edc81fb011e8d8fa1707a70d5b007d
Hirusha Adikari <hirushaadi@gmail.com>
2024-12-03 15:55:49 +0530

    Update Login.jsx

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react";
-import { login } from "../../lib/backend"
+import { login } from "../../lib/backend";
 
 const Login = () => {
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    document.title = `Login`;
+  });
 
   async function handleLogin(e) {
     e.preventDefault();
     await login(email, password);
-
   }
 
   return (
@@ -17,16 +19,30 @@ const Login = () => {
       <div className="min-h-screen">
         <div className="m-12">
           <form onSubmit={handleLogin}>
-            <input type="email" name="email" placeholder="Email" className="input input-bordered w-full max-w-xs" onChange={(e) => setEmail(e.target.value)} />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              className="input input-bordered w-full max-w-xs"
+              onChange={(e) => setEmail(e.target.value)}
+            />
             <br />
-            <input type="password" name="password" placeholder="Password" className="input input-bordered w-full max-w-xs" onChange={(e) => setPassword(e.target.value)} />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              className="input input-bordered w-full max-w-xs"
+              onChange={(e) => setPassword(e.target.value)}
+            />
             <br />
-            <button type="submit" className="btn">Login</button>
+            <button type="submit" className="btn">
+              Login
+            </button>
           </form>
         </div>
       </div>
     </>
   );
-}
+};
 
-export { Login }
+export { Login };
