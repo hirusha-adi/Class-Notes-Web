b91ebedc27d2bc9c20c7de6834f10815552f54cd
Hirusha Adikari <hirushaadi@gmail.com>
2024-12-05 09:26:46 +0530

    Update Teachers.jsx

--- src/pages/Admin/Users/Teachers.jsx
+++ src/pages/Admin/Users/Teachers.jsx
@@ -1,16 +1,21 @@
-import { Layout } from "../components/Layout"
+import { Layout } from "../components/Layout";
 
 const Teachers = () => {
-
   return (
     <>
-      <Layout pageTitle={"SLOS Statistics"} locationBreadcrumbs={["Users", "Teachers"]}>
-        <div className="m-12">
-          Stuff
+      <Layout
+        pageTitle={"SLOS Statistics"}
+        locationBreadcrumbs={["Users", "Teachers"]}
+      >
+        <div
+          className="m-12 flex justify-center items-center text-xl
+          text-gray-500 h-[25vh]"
+        >
+          Under Development
         </div>
       </Layout>
     </>
   );
-}
+};
 
-export { Teachers }
+export { Teachers };
