5d4afb2cb4467a5540259759fae2c8b05b4f2376
Hirusha Adikari <hirushaadi@gmail.com>
2024-12-05 09:27:22 +0530

    a

--- src/pages/Admin/Statistics/Statistics.jsx
+++ src/pages/Admin/Statistics/Statistics.jsx
@@ -1,16 +1,21 @@
-import { Layout } from "../components/Layout"
+import { Layout } from "../components/Layout";
 
 const Statistics = () => {
-
   return (
     <>
-      <Layout pageTitle={"SLOS Statistics"} locationBreadcrumbs={["Statistics"]}>
-        <div className="m-12">
-          Stuff
+      <Layout
+        pageTitle={"SLOS Statistics"}
+        locationBreadcrumbs={["Statistics"]}
+      >
+        <div
+          className="m-12 flex justify-center items-center text-xl
+          text-gray-500 h-[25vh]"
+        >
+          Under Development
         </div>
       </Layout>
     </>
   );
-}
+};
 
-export { Statistics }
+export { Statistics };
