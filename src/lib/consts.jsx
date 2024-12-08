a1c2fc624d0b061ce762380a61164165624b62d1
Hirusha Adikari <hirushaadi@gmail.com>
2024-12-08 21:52:29 +0530

    a

--- src/lib/consts.jsx
+++ src/lib/consts.jsx
@@ -1,4 +1,3 @@
-export const urlSidebar =
-  "https://raw.githubusercontent.com/hirusha-adi/Class-Notes-Data/refs/heads/main/sidebars/";
+export const urlSidebar = "https://db.class.hirusha.xyz/subjects/";
 
 export const tblNoOfItemsPerPage = [10, 20, 40, 50, 75, 100, 150];
