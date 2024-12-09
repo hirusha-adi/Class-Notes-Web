131488c196c438e6f74c021edb145ff9fe21a8d8
Hirusha Adikari <hirushaadi@gmail.com>
2024-12-09 08:57:57 +0530

    nice UI

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -74,37 +74,50 @@ const Login = () => {
   }
 
   return (
-    <>
-      <div className="min-h-screen">
-        <div className="m-12">
-          <form onSubmit={handleLogin}>
+    <div className="hero min-h-screen bg-base-100">
+      <div className="hero-content flex-col text-center mb-[20vh] w-full">
+        <h1 className="text-4xl font-bold mb-4">Welcome Back</h1>
+        <p className="text-lg mb-8 text-gray-500">
+          Please enter your credentials to login
+        </p>
+        <form onSubmit={handleLogin} className="w-full max-w-md">
+          <div className="form-control mb-4">
             <input
               type="email"
               name="email"
               placeholder="Email"
-              className="input input-bordered w-full max-w-xs"
+              className="input input-bordered w-full"
               onChange={(e) => setEmail(e.target.value)}
             />
-            <br />
+          </div>
+          <div className="form-control mb-6">
             <input
               type="password"
               name="password"
               placeholder="Password"
-              className="input input-bordered w-full max-w-xs"
+              className="input input-bordered w-full"
               onChange={(e) => setPassword(e.target.value)}
             />
-            <br />
-            <button className="btn" type="submit" disabled={isLoading}>
-              {isLoading ? (
-                <span className="loading loading-dots loading-lg"></span>
-              ) : (
-                "Login"
-              )}
-            </button>
-          </form>
-        </div>
+          </div>
+          <button
+            className={`btn btn-primary w-full ${
+              isLoading ? "btn-disabled" : ""
+            }`}
+            type="submit"
+            disabled={isLoading}
+          >
+            {isLoading ? (
+              <span className="loading loading-dots loading-lg"></span>
+            ) : (
+              "Login"
+            )}
+          </button>
+        </form>
+        <p className="text-sm mt-4 text-gray-400">
+          Don&apos;t have an account? Contact me at +94777662700.
+        </p>
       </div>
-    </>
+    </div>
   );
 };
 
